495c805041e4006b81000d9e752160e4009edab0
Michael Ganss <michael.ganss@updatestar.com>
2015-04-23 12:03:37 +0200

    Initial commit

---
+++ AutoLink.Tests/Tests.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ganss.Text.Tests
+{
+    public class Tests
+    {
+        public static string Linkify(string url, string text = null)
+        {
+            return string.Format(@"<a href=""{0}"">{1}</a>", url, text ?? url);
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            var s = "Check out http://www.example.com/bla?x=y#test. Over.";
+            var a = new AutoLink().Link(s);
+            Assert.AreEqual(@"Check out <a href=""http://www.example.com/bla?x=y#test"">http://www.example.com/bla?x=y#test</a>. Over.", a);
+        }
+
+        [Test]
+        public void ParensTest()
+        {
+            var url = "http://en.wikipedia.org/wiki/Link_(film)";
+            var text = "Check it out at Wikipedia ({0}).";
+
+            var a = new AutoLink().Link(string.Format(text, url));
+            Assert.AreEqual(string.Format(text, Linkify(url)), a);
+
+            text = "This film is huge [{0}].";
+            a = new AutoLink().Link(string.Format(text, url));
+            Assert.AreEqual(string.Format(text, Linkify(url)), a);
+        }
+
+        [Test]
+        public void PunctuationTest()
+        {
+            var url = "http://a.b/c?d#e";
+            var text = "Check out {0}{1}{1}{1}";
+            foreach (var p in new[] { '.', ';', '!', '?', ':', ',' })
+            {
+                var a = new AutoLink().Link(string.Format(text, url, p));
+                Assert.AreEqual(string.Format(text, Linkify(url), p), a);
+            }
+        }
+
+        [Test]
+        public void MailTest()
+        {
+            var uri = "mailto:hallo@example.com";
+            var text = "His email is {0}.";
+            var a = new AutoLink().Link(string.Format(text, uri));
+            Assert.AreEqual(string.Format(text, Linkify(uri)), a);
+        }
+
+        [Test]
+        public void WwwTest()
+        {
+            var url = "www.example.com";
+            var text = "Check it out at {0}.";
+
+            var a = new AutoLink().Link(string.Format(text, url));
+            Assert.AreEqual(string.Format(text, Linkify(url, "http://" + url)), a);
+        }
+
+        [Test]
+        public void OtherSchemesTest()
+        {
+            var url = "gopher://bla";
+            var text = "Check it out on Gopher: {0}.";
+
+            var a = new AutoLink().Link(string.Format(text, url));
+            Assert.AreEqual(string.Format(text, url), a);
+
+            var b = new AutoLink(AutoLink.DefaultSchemes.Concat(new[] { "gopher://" })).Link(string.Format(text, url));
+            Assert.AreEqual(string.Format(text, Linkify(url)), b);
+        }
+
+        [Test]
+        public void MultipleLinksTest()
+        {
+            var urls = new[] { "https://www.google.com/", "mailto:xyz@example.com", "ftp://test:test@example.com/hallo.ballo" };
+            var text = "Click here: {0}! Mail me at {1}. Download at {2}.";
+            var a = new AutoLink().Link(string.Format(text, urls));
+            Assert.AreEqual(string.Format(text, urls.Select(u => Linkify(u)).ToArray()), a);
+        }
+
+        [Test]
+        public void CustomReplaceTest()
+        {
+            var url = "http://www.example.com";
+            var text = "Check it out: {0}!";
+            Func<string, string> r = u => string.Format(@"<a class=""button"" href=""{0}"">Click here</a>", u);
+            var a = new AutoLink().Link(string.Format(text, url), r);
+            Assert.AreEqual(string.Format(text, r(url)), a);
+        }
+    }
+}
---
+++ AutoLink/AutoLink.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ganss.Text
+{
+    /// <summary>
+    /// Replaces URIs with HTML links.
+    /// </summary>
+    public class AutoLink
+    {
+        /// <summary>
+        /// The default URI prefixes which can start a URI.
+        /// </summary>
+        public static readonly ISet<string> DefaultSchemes = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { "http://", "https://", "ftp://", "mailto:", "www." };
+
+        AhoCorasick Schemes { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutoLink"/> class.
+        /// </summary>
+        /// <param name="schemes">The prefixes of URIs to recognize. If null, uses <see cref="DefaultSchemes"/>.</param>
+        public AutoLink(IEnumerable<string> schemes = null)
+        {
+            Schemes = new AhoCorasick(CharComparer.OrdinalIgnoreCase, schemes ?? DefaultSchemes);
+        }
+
+        private string DefaultReplace(string uri)
+        {
+            var text = uri.StartsWith("www.", StringComparison.OrdinalIgnoreCase) ? "http://" + uri : uri;
+            return string.Format(@"<a href=""{0}"">{1}</a>", uri, text);
+        }
+
+        /// <summary>
+        /// Replaces all recognized URIs in the specified text with HTML links, optionally using a specified replacer func.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// This method assumes that the input text does not contain HTML but only raw text.
+        /// </para>
+        /// <para>
+        /// Effort is made to exclude trailing punctuation from the end of the URI while trying to include parentheses and braces that are part of the URI.
+        /// </para>
+        /// <example>
+        /// <code>
+        /// <![CDATA[
+        /// var autolink = new AutoLink();
+        /// var text = @"Check it out at Wikipedia (http://en.wikipedia.org/wiki/Link_(film)).";
+        /// var html = autolink.Link(text);
+        /// // -> Check it out at Wikipedia (<a href="http://en.wikipedia.org/wiki/Link_(film)">http://en.wikipedia.org/wiki/Link_(film)</a>).
+        /// ]]>
+        /// </code>
+        /// </example>
+        /// </remarks>
+        /// <param name="s">The text to autolink.</param>
+        /// <param name="replace">The replacer func. Takes a URI and returns the complete HTML link.</param>
+        /// <returns>The autolinked text.</returns>
+        public string Link(string s, Func<string, string> replace = null)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return s;
+
+            var match = Schemes.Search(s).FirstOrDefault();
+            if (match.Word == null) return s;
+
+            var start = match.Index;
+            var end = FindEnd(s, start);
+
+            var r = replace ?? DefaultReplace;
+
+            return s.Substring(0, start) + r(s.Substring(start, end - start)) + Link(s.Substring(end), r);
+        }
+
+        private int FindEnd(string s, int start)
+        {
+            var parens = 0;
+            var brackets = 0;
+            var end = start;
+
+            // skip over URI characters, counting balanced parens
+            for (; end < s.Length; end++)
+            {
+                var c = s[end];
+                if (!IsUriChar(c)) break;
+                if (c == '(') parens++;
+                else if (c == ')') parens--;
+                else if (c == '[') brackets++;
+                else if (c == ']') brackets--;
+            }
+
+            // trim punctuation and unbalanced parens from end
+            while (end > start)
+            {
+                var c = s[end - 1];
+
+                if (parens < 0 && c == ')') parens++;
+                else if (brackets < 0 && c == ']') brackets++;
+                else if (!IsPunctuation(c)) break;
+
+                end--;
+            }
+
+            return end;
+        }
+
+        private bool IsPunctuation(char c)
+        {
+            return c == '!' || c == '?' || c == '.' || c == ':' || c == ';' || c == ',';
+        }
+
+        private static bool IsUriChar(char c)
+        {
+            return (c >= 'a' && c <= 'z')
+                || (c >= '?' && c <= '[')
+                || (c >= '#' && c <= ';')
+                || c == '!' || c == '=' || c == '_' || c == '~' || c == ']';
+        }
+    }
+}
